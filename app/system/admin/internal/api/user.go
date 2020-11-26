0305acd6904c935e0924b2ec1dcc6e2b48efd1b2
jflyfox <zcool321@sina.com>
2020-11-27 01:29:02 +0800

    add user profile page and nav user info

--- app/system/admin/internal/api/user.go
+++ app/system/admin/internal/api/user.go
@@ -21,3 +21,25 @@ func (a *userApi) Index(r *ghttp.Request) {
 		Title: "后台首页",
 	})
 }
+
+// @summary 展示用户管理页面
+// @tags    后台-用户管理
+// @produce html
+// @router  /admin/user/info [GET]
+// @success 200 {string} html "页面HTML"
+func (a *userApi) Info(r *ghttp.Request) {
+	// TODO 个人信息
+	service.View.Render(r, model.View{
+		Title: "个人中心",
+	})
+}
+
+// @summary 展示用户管理页面
+// @tags    后台-用户管理
+// @produce html
+// @router  /admin/user/logout [GET]
+// @success 200 {string} html "页面HTML"
+func (a *userApi) Logout(r *ghttp.Request) {
+	// TODO 登出逻辑
+	r.Response.RedirectTo("/admin/login")
+}
