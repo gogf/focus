62c56d6d39e2c6d48f7c37a80d482badc14264e1
jflyfox <zcool321@sina.com>
2020-12-31 00:42:46 +0800

    add databases sql

--- document/update.sql
+++
@@ -1,7 +0,0 @@
-/*******  20201110 用户加入唯一索引  *************/
-ALTER TABLE `gf`.`gf_user`
-ADD UNIQUE INDEX `uni_user_passport`(`passport`) USING BTREE,
-ADD UNIQUE INDEX `uni_user_nickname`(`nickname`) USING BTREE;
-
-ALTER TABLE `gf`.`gf_content`
-    ADD INDEX `idx_content_user_id`(`user_id`) USING BTREE;
